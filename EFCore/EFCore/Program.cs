5568600e588831891155fc5e916d13de1a9eec9a
이득유 <ldy8070>
2022-11-30 18:09:54 +0900

    command
    command

--- EFCore/EFCore/Program.cs
+++ EFCore/EFCore/Program.cs
@@ -6,23 +6,36 @@ namespace EFCore
 {
     class Program
     {
-        // 초기화 시간이 좀 걸림
-        static void InitializeDB(bool forceReset = false)
-        {
-            using (AppDbContext db = new AppDbContext()) 
-            {
-                if (!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
-                    return;
-
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                Console.WriteLine("DB Initialized");
-            }
-        }
         static void Main(string[] args)
         {
-            InitializeDB(true);
+            DbCommands.InitializeDB(false);
+
+            // CRUD (Create-Read-Update-Delete)
+            Console.WriteLine("명령어를 입력하세요.");
+            Console.WriteLine("[0] Force Reset");
+            Console.WriteLine("[1] ReadAll");
+            Console.WriteLine("[2] UpdateDate");
+            Console.WriteLine("[3] DeleteItem");
+            while (true)
+            {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                switch(command)
+                {
+                    case "0":
+                        DbCommands.InitializeDB(true);
+                        break;
+                    case "1":
+                        DbCommands.ReadAll();
+                        break;
+                    case "2":
+                        DbCommands.UpdateDate();
+                        break;
+                    case "3":
+                        DbCommands.DeleteItem();
+                        break;
+                }
+            }
         }
     }
 }
